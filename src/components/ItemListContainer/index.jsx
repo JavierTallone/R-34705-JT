b24cbee583bf33f912d834d722c8eb83738a1c4e
JavierTallone <93626346+JavierTallone@users.noreply.github.com>
2022-08-02 19:40:33 -0300

    Delete src directory

--- src/components/ItemListContainer/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Heading } from '@chakra-ui/react'
-const ItemListContainer = ({greeting}) => {
-
-    return(
-        <Heading>{greeting}</Heading>
-    )
-}
-
-export { ItemListContainer }
